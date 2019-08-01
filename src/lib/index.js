ac05092aad122ca7195f530db9ff7bd14cc37d4d
MercedesDavila <mmdavila@outlook.com>
2019-08-01 14:32:22 -0500

    Maquetación homepage y login

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,16 @@
 // aqui exportaras las funciones que necesites
+const loginButton = document.getElementById('login-button');
+const registerButton = document.getElementById('register-button');
 
-export const myFunction = () => {
-  // aqui tu codigo
-  console.log('Hola mundo!');
-};
+
+//function open or close sections
+const showSection = id => document.getElementById(id).classList.remove('hide');
+const hideSection = id => document.getElementById(id).classList.add('hide');
+
+//functions to change sections
+loginButton.addEventListener("click", goinglogin);
+
+const goinglogin = () => {
+    hideSection('welcomepage');
+    showSection('login');
+}
