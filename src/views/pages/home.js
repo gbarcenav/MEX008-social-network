af6c612d8e50daa4319e5957e044ff5f32dfde6c
Gabriela Barcena <g.barcena.v@gmail.com>
2019-08-18 19:43:34 -0500

    Agregamos media queries para sección HOME

--- src/views/pages/home.js
+++ src/views/pages/home.js
@@ -26,10 +26,12 @@ let Home = {
     let view = /*html*/ `
     <section class="home-section" id="welcomepage">
       <div class="home-elements">
-      <figure><img src="./img/logo_corregido.png" alt="Logo SupportMe"></figure>
+      <figure><img src="./img/logo_corregido.png" srcset="./img/logo-2.png 600w" alt="Logo SupportMe"></figure>
       <div class="text-information">La red social que te ayudará a mantenerte informada acerca del nuevo lugar donde vivirás haciendo más fácil el proceso de adaptación a tu nuevo hogar. Además podrás mantener el contacto con tus amigas, vecinas y/o conocidas.</div>
+      <div class="two-btn">
       <div class="btn-simulation"><a id="login-button" href="#/login">Iniciar sesión</a></div>
       <div class="btn-simulation"><a id="register-button" href="#/register">RegistrarMe</a></div>
+      </div>
     </div>
     </section>
          `;
